15d09421b01f3065cdae7a9b9b588b3762487cc5
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-10-26 17:14:30 +0100

    add header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Header</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    
+
+</head>
+<body>
+
+    <header id="app_header">
+
+    </header>
+    @include('partials.header')
+
+    <main>
+    {{--
+        @yield('content')
+        --}}
+    </main>
+    {{--
+    @include('partials.footer')
+    --}}
+    <footer>
+
+    </footer>
+
+</body>
+</html>
+
+{{--
+    <nav>
+        <ul>
+            <li><a href="{{ url('/') }}">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contacts">Contacts</a></li>
+        </ul>
+    </nav>
+    <h1>Hello World</h1>
+    <h1>{{$name}}</h1>
+    <p>You are {{ $age }} years old.</p>
+    --}}
+    {{-- --}}
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+<header id="app_header">
+
+            <div class="nav_img">
+            {{-- <img src="../../../src/assets/img/dc-logo.png" width="60px" alt="">--}}
+            </div>
+            <div class="nav_tags">
+                <div><a href="#">CHARACTERS</a></div>
+                <div class="active"><a  href="#">COMICS</a></div>
+                <div><a href="#">MOVIES</a></div>
+                <div><a href="#">TV</a></div>
+                <div><a href="#">GAMES</a></div>
+                <div><a href="#">COLLECTIBLES</a></div>
+                <div><a href="#">VIDEOS</a></div>
+                <div><a href="#">FANS</a></div>
+                <div><a href="#">NEWS</a></div>
+                <div><a href="#">SHOP</a></div>
+            </div>
+          
+    </header>
+
+    
